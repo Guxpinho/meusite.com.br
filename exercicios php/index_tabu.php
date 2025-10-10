9ce5f60b3cf5edcb15ea74075f6a555ebebb7af6
Gustavo Pinho <gustavo.r.pinho@aluno.senai.br>
2025-10-10 13:33:54 -0300

    26/09/25

---
+++ exercicios php/index_tabu.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calcular o imc</title>
+</head>
+<body>
+    <h1>Calcular o IMC</h1>
+    <form action="IMC.php" method="post">
+        <label for="nome">Qual é </label>
+        <input type="number" id="number" name="number" required>
+
+    </form>
+    
+</body>
+</html>
